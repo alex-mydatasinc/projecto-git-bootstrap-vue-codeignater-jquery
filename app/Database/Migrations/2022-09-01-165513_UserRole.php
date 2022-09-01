3bbfd426a1014dea0748e2c97a35fdef3180a822
yair1215 <yairyousantiago@gmail.com>
2022-09-01 13:55:00 -0500

    develop

---
+++ app/Database/Migrations/2022-09-01-165513_UserRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UserRole extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'user_id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => TRUE
+            ],
+            'role_id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => TRUE
+
+
+            ]
+        ]);
+        $this->forge->addForeignKey('user_id', users, 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('role_id', roles, 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('user_role');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user_role');
+    }
+}
